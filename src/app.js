20bb7f407b2512fd819f079601b9e69570b3f037
Simon Boyd <sboyd@companieshouse.gov.uk>
2022-03-24 16:16:52 +0000

    Resolves:

--- src/app.js
+++ src/app.js
@@ -10,8 +10,30 @@ app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+const accountData = fs.readFileSync('src/json/accounts.json', { encoding: 'utf8', flag: 'r' });
+const accounts = JSON.parse(accountData);
+
+const userData = fs.readFileSync('src/json/users.json', { encoding: 'utf8', flag: 'r' });
+const users = JSON.parse(userData);
+
+app.get('/checking', (req, res) => {
+  res.render('account', { account: accounts.checking });
+});
+
+app.get('/savings', (req, res) => {
+  res.render('account', { account: accounts.savings });
+});
+
+app.get('/credit', (req, res) => {
+  res.render('account', { account: accounts.credit });
+});
+
+app.get('/profile', (req, res) => {
+  res.render('profile', { user: users[0] });
+});
+
 app.get('/', (req, res) => {
-  res.render('index', { title: 'Index' });
+  res.render('index', { title: 'Account Summary', accounts: accounts });
 });
 
 app.listen(3000, () => {
