20e06e86e0c851c28a2fc302c20005ee84e461f3
Simon Boyd <sboyd@companieshouse.gov.uk>
2022-03-24 17:10:03 +0000

    Resolves:

--- src/app.js
+++ src/app.js
@@ -9,11 +9,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.urlencoded({ extended: true }));
 
-const accountData = fs.readFileSync('src/json/accounts.json', { encoding: 'utf8', flag: 'r' });
+const accountData = fs.readFileSync('src/json/accounts.json', 'utf8');
 const accounts = JSON.parse(accountData);
 
-const userData = fs.readFileSync('src/json/users.json', { encoding: 'utf8', flag: 'r' });
+const userData = fs.readFileSync('src/json/users.json', { encoding: 'utf8' });
 const users = JSON.parse(userData);
 
 app.get('/checking', (req, res) => {
@@ -30,6 +31,40 @@ app.get('/credit', (req, res) => {
 
 app.get('/profile', (req, res) => {
   res.render('profile', { user: users[0] });
+});
+
+app.get('/transfer', (req, res) => {
+  res.render('transfer');
+});
+
+app.post('/transfer', (req, res) => {
+  const amount = parseInt(req.body.amount);
+
+  accounts[req.body.from].balance -= amount;
+  accounts[req.body.to].balance += amount;
+
+  const accountsJSON = JSON.stringify(accounts);
+
+  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+
+  res.render('transfer', { message: 'Transfer Completed' });
+});
+
+app.get('/payment', (req, res) => {
+  res.render('payment', { account: accounts.credit });
+});
+
+app.post('/payment', (req, res) => {
+  const amount = parseInt(req.body.amount);
+
+  accounts.credit.balance -= amount;
+  accounts.credit.available += amount;
+
+  const accountsJSON = JSON.stringify(accounts);
+
+  fs.writeFileSync(path.join(__dirname, 'json/accounts.json'), accountsJSON, 'utf8');
+
+  res.render('payment', { message: 'Payment Successful', account: accounts.credit });
 });
 
 app.get('/', (req, res) => {
